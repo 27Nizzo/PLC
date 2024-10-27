37650974643ba1018c8b807fe521d59f13541fd1
Luis Silva <biokiller134@gmail.com>
2024-10-27 19:34:53 +0000

    Só falta nome por séculos no html

--- TP1/Ex1/Ex1.py
+++ TP1/Ex1/Ex1.py
@@ -1,4 +1,5 @@
 import re
+import json
 ## opcional
 
 def TextCleanup():
@@ -73,7 +74,7 @@ lines = file.readlines()
 
 ## alínea a)
 def YearlyFrequency():
-    years = {}
+    years = { "anos" : {} }
 
     for pr in lines:
         data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})',pr)
@@ -83,22 +84,25 @@ def YearlyFrequency():
             date_components = re.split(r'-', date)
             year = date_components[0]
 
-            if year not in years:
-                years[year] = 1
+            if year not in years["anos"]:
+                years["anos"][year] = 1
             else:
-                years[year] += 1
+                years["anos"][year] += 1
 
-    return years
+    json_object = json.dumps(years, indent=4)
+
+    with open("YearlyFrequency.json", "w") as outfile:
+        outfile.write(json_object)
 
 ## alínea b)
 def NameFrequency():
-    centuries = {}
+    centuries = { "seculos" : {} }
     fn = "FirstName"
     ln = "LastName"
 
     for pr in lines:
-        data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}',pr)
-        names = re.findall('([a-z|A-Z| ]+[:]{2})',pr)
+        data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})',pr)
+        names = re.findall('[:]{2}([a-z|A-Z| ]+[:]{2})',pr)
 
         if data != None and names != None:
             date = data.group(2)
@@ -106,30 +110,37 @@ def NameFrequency():
             year = date_components[0]
             century = int(int(year)/100)
 
-            if century not in centuries:
-                centuries[century] = {}
-                centuries[century][fn] = {}
-                centuries[century][ln] = {}
+            if century not in centuries["seculos"]:
+                centuries["seculos"][century] = {}
+                centuries["seculos"][century][fn] = {}
+                centuries["seculos"][century][ln] = {}
+                centuries["seculos"][century]["nomes"] = []
 
             for name in names:
-                listNames = re.split('[ |:]',name)
+                if name not in centuries["seculos"][century]["nomes"] and name[0] != "," :
+                    centuries["seculos"][century]["nomes"].append(name)
+                    listNames = re.split('[ |:]',name)
+                    listNames.pop()
 
-                if listNames[0] != "":
-                    if listNames[0] not in centuries[century][fn]:
-                        centuries[century][fn][listNames[0]] = 1
-                    else:
-                        centuries[century][fn][listNames[0]] += 1
+                    if listNames[0] != "":
+                        if listNames[0] not in centuries["seculos"][century][fn]:
+                            centuries["seculos"][century][fn][listNames[0]] = 1
+                        else:
+                            centuries["seculos"][century][fn][listNames[0]] += 1
 
-                    if listNames[len(listNames)-2] not in centuries[century][ln]:
-                        centuries[century][ln][listNames[len(listNames)-2]] = 1
-                    else:
-                        centuries[century][ln][listNames[len(listNames)-2]] += 1
+                        if listNames[len(listNames)-2] not in centuries["seculos"][century][ln]:
+                            centuries["seculos"][century][ln][listNames[len(listNames)-2]] = 1
+                        else:
+                            centuries["seculos"][century][ln][listNames[len(listNames)-2]] += 1
 
-    return centuries
+    json_object = json.dumps(centuries, indent=4)
+
+    with open("NameFrequency.json", "w") as outfile:
+        outfile.write(json_object)
 
 ## alínea c) 
 def RecommendedFrequency():
-    recommended = {}
+    recommended = { "recomendacoes" : {} }
 
     for pr in lines:
         data = re.findall('([A-Z][a-zA-Z ]+),([a-zA-Z ]+)\. ?(?i:(Proc\.[0-9]+))',pr)
@@ -137,20 +148,24 @@ def RecommendedFrequency():
             familiar = []
             for d in data:
                 referente = d[1]
-                if referente not in recommended:
+                if referente not in recommended["recomendacoes"]:
                     familiar.append(referente)
-                    recommended[referente] = 1
+                    recommended["recomendacoes"][referente] = 1
                 elif referente not in familiar:
                     familiar.append(referente)
-                    recommended[referente] += 1
+                    recommended["recomendacoes"][referente] += 1
     
-    return recommended
+    json_object = json.dumps(recommended, indent=4)
+
+    with open("RecommendedFrequency.json", "w") as outfile:
+        outfile.write(json_object)
 
 ## alinea d)
 
 def MoreThanOneChildFrequency():
 
     progenitores = {}
+    morethan = { "child" : {} }
 
     for pr in lines:
         data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}',pr)
@@ -173,7 +188,14 @@ def MoreThanOneChildFrequency():
             if confessado not in progenitores[mae]:
                 progenitores[mae].append(confessado)
 
-    return progenitores
+    for pai in progenitores:
+        if len(progenitores[pai]) > 1:
+            morethan["child"][pai] = progenitores[pai]
+
+    json_object = json.dumps(morethan, indent=4)
+
+    with open("MoreThanOneChildFrequency.json", "w") as outfile:
+        outfile.write(json_object)
 
 TextCleanup()
 
@@ -187,19 +209,18 @@ MoreThanOneChildFrequency()
 
 # alinea e)
 
-import json
-
 def PrintToJson():
     processo = re.search("([0-9]+)[:]{2}([0-9]{4}-[0-9]{2}-[0-9]{2})[:]{2}(.+)",lines[1])
     componentesSemData = re.split("::",processo.group(3))
     componentesSemData.pop()
     processo_json = {
-        "Numero" : processo.group(1),
-        "Data" : processo.group(2),
-        "Confessado" : componentesSemData[0],
-        "Pai" : componentesSemData[1],
-        "Mae" : componentesSemData[2],
-        "Observacoes" : componentesSemData[3]
+        "Processo " + processo.group(1) : {
+            "Data" : processo.group(2),
+            "Confessado" : componentesSemData[0],
+            "Pai" : componentesSemData[1],
+            "Mae" : componentesSemData[2],
+            "Observacoes" : componentesSemData[3]
+        },
     }
 
     json_object = json.dumps(processo_json, indent=4)
