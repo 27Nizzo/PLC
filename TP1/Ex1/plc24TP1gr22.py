7d12a57fca5a27bed617216e1102cd7650ec6992
27Nizzo <afonso7557@gmail.com>
2024-11-15 15:34:00 +0000

    codigo explicado

--- TP1/Ex1/plc24TP1gr22.py
+++ TP1/Ex1/plc24TP1gr22.py
@@ -3,41 +3,53 @@ import json
 ## opcional
 
 def TextCleanup():
+    # Abre o arquivo 'processos.txt' em modo de leitura para processar os dados originais
     file = open("processos.txt","r")
     lines = file.readlines()
+    
+    # Cria um novo arquivo 'processosTrimmed.txt' em modo de escrita para salvar dados limpos
     clean = open("processosTrimmed.txt","w")
-    clean.write("NumProc::Data::Confessado::Pai::Mae::Observacoes::\n")
-    processos = {}
-    processosRealocar = []
-    maxProcessNumber = 0
+    clean.write("NumProc::Data::Confessado::Pai::Mae::Observacoes::\n") # Escreve o cabeçalho
 
+    processos = {}  # Dicionário para armazenar processos únicos
+    processosRealocar = []  # Lista para armazenar processos a serem realocados
+    maxProcessNumber = 0  # Variável para rastrear o maior número de processo
+
+    # Loop para processar cada linha no arquivo original
     for pr in lines:
+        # Usa regex para identificar o número do processo, data e pessoas envolvidas
         process = re.search("([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}(.+)",pr)
+        
         if process != None:
-            processNumber = process.group(1)
-            processData = re.split(r'-',process.group(2))
-            processPeople = re.split(r'::',process.group(3))
-            processPeople.pop()
+            processNumber = process.group(1)  # Número do processo
+            processData = re.split(r'-',process.group(2))  # Divide a data em ano, mês e dia
+            processPeople = re.split(r'::',process.group(3))  # Divide as informações de pessoas
+            processPeople.pop()  # Remove o último elemento em branco
 
+            # Atualiza o maior número de processo, se necessário
             if int(processNumber) > maxProcessNumber:
                 maxProcessNumber = int(processNumber)
 
+            # Se o número do processo não existe no dicionário, cria uma nova entrada
             if processNumber not in processos:
-                processos[processNumber] = {}
-                processos[processNumber]["string"] = pr
-                processos[processNumber]["ano"] = processData[0]
-                processos[processNumber]["mes"] = processData[1]
-                processos[processNumber]["dia"] = processData[2]
-                processos[processNumber]["pessoas"] = processPeople
+                processos[processNumber] = {
+                    "string": pr,
+                    "ano": processData[0],
+                    "mes": processData[1],
+                    "dia": processData[2],
+                    "pessoas": processPeople
+                }
             
+            # Caso o processo já exista, compara as datas e substitui se a nova é anterior
             elif processos[processNumber]["ano"] > processData[0]:
-                processosRealocar.append(processos[processNumber]["string"])
+                processosRealocar.append(processos[processNumber]["string"])  # Move o processo existente para realocação
                 processos[processNumber]["string"] = pr
                 processos[processNumber]["ano"] = processData[0]
                 processos[processNumber]["mes"] = processData[1]
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
             
+            # Segue a mesma lógica para meses e dias
             elif processos[processNumber]["mes"] > processData[1]:
                 processosRealocar.append(processos[processNumber]["string"])
                 processos[processNumber]["string"] = pr
@@ -45,7 +57,6 @@ def TextCleanup():
                 processos[processNumber]["mes"] = processData[1]
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
-
             elif processos[processNumber]["dia"] > processData[2]:
                 processosRealocar.append(processos[processNumber]["string"])
                 processos[processNumber]["string"] = pr
@@ -54,8 +65,10 @@ def TextCleanup():
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
             elif processos[processNumber]["pessoas"][0] != processPeople[0] and processos[processNumber]["pessoas"][1] != processPeople[1] and processos[processNumber]["pessoas"][2] != processPeople[2]:
+                # Se as pessoas listadas não coincidem, move o processo para realocação
                 processosRealocar.append(pr)
 
+    # Escreve cada processo limpo no arquivo final
     for pr in processos:
         processo = pr + "::" + processos[pr]["ano"] + "-" + processos[pr]["mes"] + "-" + processos[pr]["dia"] + "::"
         for person in processos[pr]["pessoas"]:
@@ -63,20 +76,22 @@ def TextCleanup():
         processo = processo + "\n"
         clean.write(processo)
     
+    # Realoca processos em números novos
     for pr in processosRealocar:
         process = re.search("([0-9]+)(.+)",pr)
         maxProcessNumber += 1
         newProcess = str(maxProcessNumber) + process.group(2) + "\n"
         clean.write(newProcess)
 
-file = open("processosTrimmed.txt","r")
-lines = file.readlines()
+file = open("processosTrimmed.txt","r")  # Reabre o arquivo final para outras funções
+lines = file.readlines()  # Lê todas as linhas para análise
 
-## alínea a)
+## Função para contar processos por ano
 def YearlyFrequency():
     years = {}
 
     for pr in lines:
+        # Extrai o ano da data usando regex
         data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})',pr)
 
         if data != None:
@@ -89,11 +104,11 @@ def YearlyFrequency():
             else:
                 years[year] += 1
 
-    json_object = json.dumps(years, indent=4)
+    json_object = json.dumps(years, indent=4)  # Converte para JSON
 
-    return json_object
+    return json_object  # Retorna o JSON para visualização
 
-## alínea b)
+## Função para calcular frequência de nomes por século
 def NameFrequency():
     centuries = {}
     fn = "FirstName"
@@ -110,13 +125,14 @@ def NameFrequency():
             century = int(int(year)/100)
 
             if century not in centuries:
-                centuries[century] = {}
-                centuries[century][fn] = {}
-                centuries[century][ln] = {}
-                centuries[century]["nomes"] = []
+                centuries[century] = {
+                    fn: {},
+                    ln: {},
+                    "nomes": []
+                }
 
             for name in names:
-                if name not in centuries[century]["nomes"] and name[0] != "," :
+                if name not in centuries[century]["nomes"] and name[0] != ",":
                     centuries[century]["nomes"].append(name)
                     listNames = re.split('[ |:]',name)
                     listNames.pop()
@@ -127,74 +143,18 @@ def NameFrequency():
                         else:
                             centuries[century][fn][listNames[0]] += 1
 
-                        if listNames[len(listNames)-2] not in centuries[century][ln]:
-                            centuries[century][ln][listNames[len(listNames)-2]] = 1
+                        if listNames[-2] not in centuries[century][ln]:
+                            centuries[century][ln][listNames[-2]] = 1
                         else:
-                            centuries[century][ln][listNames[len(listNames)-2]] += 1
+                            centuries[century][ln][listNames[-2]] += 1
 
     for c in centuries:
         centuries[c].pop("nomes")
 
     json_object = json.dumps(centuries, indent=4)
 
-    return json_object
+    return json_object  # Retorna o JSON para visualização
 
-## alínea c) 
-def RecommendedFrequency():
-    recommended = {}
-
-    for pr in lines:
-        data = re.findall('([A-Z][a-zA-Z ]+),([a-zA-Z ]+)\. ?(?i:(Proc\.[0-9]+))',pr)
-        if data != None:
-            familiar = []
-            for d in data:
-                referente = d[1]
-                if referente not in recommended:
-                    familiar.append(referente)
-                    recommended[referente] = 1
-                elif referente not in familiar:
-                    familiar.append(referente)
-                    recommended[referente] += 1
-    
-    json_object = json.dumps(recommended, indent=4)
-
-    return json_object
-
-## alinea d)
-
-def MoreThanOneChildFrequency():
-
-    progenitores = {}
-    morethan = {}
-
-    for pr in lines:
-        data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}',pr)
-        names = re.search('([a-z|A-Z| ]+)[:]{2}([a-z|A-Z| ]+)[:]{2}([a-z|A-Z| ]+)[:]{2}',pr)
-        
-        if data != None and names != None:
-            confessado = names.group(1)
-            pai = names.group(2)
-            mae = names.group(3)
-
-            if pai not in progenitores:
-                progenitores[pai] = []
-            
-            if confessado not in progenitores[pai]:
-                progenitores[pai].append(confessado)
-            
-            if mae not in progenitores:
-                progenitores[mae] = []
-            
-            if confessado not in progenitores[mae]:
-                progenitores[mae].append(confessado)
-
-    for pai in progenitores:
-        if len(progenitores[pai]) > 1:
-            morethan[pai] = progenitores[pai]
-
-    json_object = json.dumps(morethan, indent=4)
-
-    return json_object
 
 # alinea e)
 
