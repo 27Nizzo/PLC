b85d70dd043775a0b516bc7aeca2886912971c79
27Nizzo <afonso7557@gmail.com>
2024-11-15 17:36:44 +0000

    correção

--- TP1/Ex1/plc24TP1gr22.py
+++ TP1/Ex1/plc24TP1gr22.py
@@ -3,53 +3,41 @@ import json
 ## opcional
 
 def TextCleanup():
-    # Abre o arquivo 'processos.txt' em modo de leitura para processar os dados originais
     file = open("processos.txt","r")
     lines = file.readlines()
-    
-    # Cria um novo arquivo 'processosTrimmed.txt' em modo de escrita para salvar dados limpos
     clean = open("processosTrimmed.txt","w")
-    clean.write("NumProc::Data::Confessado::Pai::Mae::Observacoes::\n") # Escreve o cabeçalho
+    clean.write("NumProc::Data::Confessado::Pai::Mae::Observacoes::\n")
+    processos = {}
+    processosRealocar = []
+    maxProcessNumber = 0
 
-    processos = {}  # Dicionário para armazenar processos únicos
-    processosRealocar = []  # Lista para armazenar processos a serem realocados
-    maxProcessNumber = 0  # Variável para rastrear o maior número de processo
-
-    # Loop para processar cada linha no arquivo original
     for pr in lines:
-        # Usa regex para identificar o número do processo, data e pessoas envolvidas
         process = re.search("([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}(.+)",pr)
-        
         if process != None:
-            processNumber = process.group(1)  # Número do processo
-            processData = re.split(r'-',process.group(2))  # Divide a data em ano, mês e dia
-            processPeople = re.split(r'::',process.group(3))  # Divide as informações de pessoas
-            processPeople.pop()  # Remove o último elemento em branco
+            processNumber = process.group(1)
+            processData = re.split(r'-',process.group(2))
+            processPeople = re.split(r'::',process.group(3))
+            processPeople.pop()
 
-            # Atualiza o maior número de processo, se necessário
             if int(processNumber) > maxProcessNumber:
                 maxProcessNumber = int(processNumber)
 
-            # Se o número do processo não existe no dicionário, cria uma nova entrada
             if processNumber not in processos:
-                processos[processNumber] = {
-                    "string": pr,
-                    "ano": processData[0],
-                    "mes": processData[1],
-                    "dia": processData[2],
-                    "pessoas": processPeople
-                }
-            
-            # Caso o processo já exista, compara as datas e substitui se a nova é anterior
-            elif processos[processNumber]["ano"] > processData[0]:
-                processosRealocar.append(processos[processNumber]["string"])  # Move o processo existente para realocação
+                processos[processNumber] = {}
                 processos[processNumber]["string"] = pr
                 processos[processNumber]["ano"] = processData[0]
                 processos[processNumber]["mes"] = processData[1]
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
             
-            # Segue a mesma lógica para meses e dias
+            elif processos[processNumber]["ano"] > processData[0]:
+                processosRealocar.append(processos[processNumber]["string"])
+                processos[processNumber]["string"] = pr
+                processos[processNumber]["ano"] = processData[0]
+                processos[processNumber]["mes"] = processData[1]
+                processos[processNumber]["dia"] = processData[2]
+                processos[processNumber]["pessoas"] = processPeople
+            
             elif processos[processNumber]["mes"] > processData[1]:
                 processosRealocar.append(processos[processNumber]["string"])
                 processos[processNumber]["string"] = pr
@@ -57,6 +45,7 @@ def TextCleanup():
                 processos[processNumber]["mes"] = processData[1]
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
+
             elif processos[processNumber]["dia"] > processData[2]:
                 processosRealocar.append(processos[processNumber]["string"])
                 processos[processNumber]["string"] = pr
@@ -65,10 +54,8 @@ def TextCleanup():
                 processos[processNumber]["dia"] = processData[2]
                 processos[processNumber]["pessoas"] = processPeople
             elif processos[processNumber]["pessoas"][0] != processPeople[0] and processos[processNumber]["pessoas"][1] != processPeople[1] and processos[processNumber]["pessoas"][2] != processPeople[2]:
-                # Se as pessoas listadas não coincidem, move o processo para realocação
                 processosRealocar.append(pr)
 
-    # Escreve cada processo limpo no arquivo final
     for pr in processos:
         processo = pr + "::" + processos[pr]["ano"] + "-" + processos[pr]["mes"] + "-" + processos[pr]["dia"] + "::"
         for person in processos[pr]["pessoas"]:
@@ -76,22 +63,20 @@ def TextCleanup():
         processo = processo + "\n"
         clean.write(processo)
     
-    # Realoca processos em números novos
     for pr in processosRealocar:
         process = re.search("([0-9]+)(.+)",pr)
         maxProcessNumber += 1
         newProcess = str(maxProcessNumber) + process.group(2) + "\n"
         clean.write(newProcess)
 
-file = open("processosTrimmed.txt","r")  # Reabre o arquivo final para outras funções
-lines = file.readlines()  # Lê todas as linhas para análise
+file = open("processosTrimmed.txt","r")
+lines = file.readlines()
 
-## Função para contar processos por ano
+## alínea a)
 def YearlyFrequency():
     years = {}
 
     for pr in lines:
-        # Extrai o ano da data usando regex
         data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})',pr)
 
         if data != None:
@@ -104,11 +89,11 @@ def YearlyFrequency():
             else:
                 years[year] += 1
 
-    json_object = json.dumps(years, indent=4)  # Converte para JSON
+    json_object = json.dumps(years, indent=4)
 
-    return json_object  # Retorna o JSON para visualização
+    return json_object
 
-## Função para calcular frequência de nomes por século
+## alínea b)
 def NameFrequency():
     centuries = {}
     fn = "FirstName"
@@ -125,14 +110,13 @@ def NameFrequency():
             century = int(int(year)/100)
 
             if century not in centuries:
-                centuries[century] = {
-                    fn: {},
-                    ln: {},
-                    "nomes": []
-                }
+                centuries[century] = {}
+                centuries[century][fn] = {}
+                centuries[century][ln] = {}
+                centuries[century]["nomes"] = []
 
             for name in names:
-                if name not in centuries[century]["nomes"] and name[0] != ",":
+                if name not in centuries[century]["nomes"] and name[0] != "," :
                     centuries[century]["nomes"].append(name)
                     listNames = re.split('[ |:]',name)
                     listNames.pop()
@@ -143,18 +127,74 @@ def NameFrequency():
                         else:
                             centuries[century][fn][listNames[0]] += 1
 
-                        if listNames[-2] not in centuries[century][ln]:
-                            centuries[century][ln][listNames[-2]] = 1
+                        if listNames[len(listNames)-2] not in centuries[century][ln]:
+                            centuries[century][ln][listNames[len(listNames)-2]] = 1
                         else:
-                            centuries[century][ln][listNames[-2]] += 1
+                            centuries[century][ln][listNames[len(listNames)-2]] += 1
 
     for c in centuries:
         centuries[c].pop("nomes")
 
     json_object = json.dumps(centuries, indent=4)
 
-    return json_object  # Retorna o JSON para visualização
+    return json_object
 
+## alínea c) 
+def RecommendedFrequency():
+    recommended = {}
+
+    for pr in lines:
+        data = re.findall('([A-Z][a-zA-Z ]+),([a-zA-Z ]+)\. ?(?i:(Proc\.[0-9]+))',pr)
+        if data != None:
+            familiar = []
+            for d in data:
+                referente = d[1]
+                if referente not in recommended:
+                    familiar.append(referente)
+                    recommended[referente] = 1
+                elif referente not in familiar:
+                    familiar.append(referente)
+                    recommended[referente] += 1
+    
+    json_object = json.dumps(recommended, indent=4)
+
+    return json_object
+
+## alinea d)
+
+def MoreThanOneChildFrequency():
+
+    progenitores = {}
+    morethan = {}
+
+    for pr in lines:
+        data = re.search('([0-9]+)[:]{2}([0-9]{4}\-[0-9]{2}\-[0-9]{2})[:]{2}',pr)
+        names = re.search('([a-z|A-Z| ]+)[:]{2}([a-z|A-Z| ]+)[:]{2}([a-z|A-Z| ]+)[:]{2}',pr)
+        
+        if data != None and names != None:
+            confessado = names.group(1)
+            pai = names.group(2)
+            mae = names.group(3)
+
+            if pai not in progenitores:
+                progenitores[pai] = []
+            
+            if confessado not in progenitores[pai]:
+                progenitores[pai].append(confessado)
+            
+            if mae not in progenitores:
+                progenitores[mae] = []
+            
+            if confessado not in progenitores[mae]:
+                progenitores[mae].append(confessado)
+
+    for pai in progenitores:
+        if len(progenitores[pai]) > 1:
+            morethan[pai] = progenitores[pai]
+
+    json_object = json.dumps(morethan, indent=4)
+
+    return json_object
 
 # alinea e)
 
@@ -217,49 +257,49 @@ if __name__ == "__main__":
                         document.getElementById("table2").innerHTML = "";
                         document.getElementById("table3").innerHTML = "";
                         const l = Object.keys(choice).length;
-                        text += `<select onchange="choose_parent(this.value)" >`
-                        text += `<option value="">Choose an option:</option>`
+                        text += <select onchange="choose_parent(this.value)" >
+                        text += <option value="">Choose an option:</option>
                             for (let y in choice){
-                                text += `<option value="${choice[y]}"> ${y} </option>` 
+                                text += <option value="${choice[y]}"> ${y} </option> 
                             }
                         text += "</select>";
-                        text += ` ---  Número de Pais com mais de um Filho Confessado : ${l}`
+                        text +=  ---  Número de Pais com mais de um Filho Confessado : ${l}
                     }
 
                     else {
                         if (select == "YearlyFrequency"){
                             document.getElementById("table2").innerHTML = "";
                             document.getElementById("table3").innerHTML = "";
-                            text += `Ano <select onchange="choose_year(this.value)" >`
-                            text += `<option value="0">Choose an option:</option>`
+                            text += Ano <select onchange="choose_year(this.value)" >
+                            text += <option value="0">Choose an option:</option>
                             for (let y in choice){
-                                text += `<option value="${choice[y]}"> ${y} </option>`
+                                text += <option value="${choice[y]}"> ${y} </option>
                             }
-                            text += `</select> `;
+                            text += </select> ;
                         }
                         else {
                             if (select == "NameFrequency"){
                             document.getElementById("table2").innerHTML = "";
                             document.getElementById("table3").innerHTML = "";
-                            text += `Século <select onchange="choose_FLname(this.value)" >`
-                            text += `<option value="">Choose an option:</option>`
+                            text += Século <select onchange="choose_FLname(this.value)" >
+                            text += <option value="">Choose an option:</option>
                             for (let y in choice){
                                 console.log(choice[y])
-                                text += `<option value=${JSON.stringify(choice[y])}> ${y}</option>`
+                                text += <option value=${JSON.stringify(choice[y])}> ${y}</option>
                             }
-                            text += `</select> `;
+                            text += </select> ;
                             }
                             
                             else {
                                 if (select == "RecommendedFrequency"){
                                     document.getElementById("table2").innerHTML = "";
                                     document.getElementById("table3").innerHTML = "";
-                                    text += `Recomendador <select onchange="choose_recomended(this.value)" >`
-                                    text += `<option value="0">Choose an option:</option>`
+                                    text += Recomendador <select onchange="choose_recomended(this.value)" >
+                                    text += <option value="0">Choose an option:</option>
                                     for (let y in choice){
-                                        text += `<option value="${choice[y]}"> ${y} </option>`
+                                        text += <option value="${choice[y]}"> ${y} </option>
                                     }
-                                    text += `</select> `;
+                                    text += </select> ;
                                 }
                             }
                         }
@@ -272,18 +312,18 @@ if __name__ == "__main__":
                 text = "Número de Filhos Confessados : " + array.length.toString() + "<br />" + "<br />";
                 
                 for (let f in array){
-                    text += `${array[f]}, <br />`
+                    text += ${array[f]}, <br />
                 }
 
                 document.getElementById("table2").innerHTML = text;
             }
 
             function choose_year(select){
-                document.getElementById("table2").innerHTML = `Número de processos :: ${select}`;
+                document.getElementById("table2").innerHTML = Número de processos :: ${select};
             }
 
             function choose_recomended(select){
-                document.getElementById("table2").innerHTML = `Número de processos recomendados :: ${select}`;
+                document.getElementById("table2").innerHTML = Número de processos recomendados :: ${select};
             }
 
             function choose_FLname(select){
@@ -293,11 +333,11 @@ if __name__ == "__main__":
                 console.log(obj)
                 let text = "";
 
-                text = `Nomes Próprios ou Apelidos: <select onchange="choose_name(this.value)" >`;
-                text += `<option value="0">Choose an option:</option>`;
-                text += `<option value=${JSON.stringify(obj[fn])}> Nomes Próprios </option>`;
-                text += `<option value=${JSON.stringify(obj[ln])}> Apelidos </option>`;
-                text += `</select> `;
+                text = Nomes Próprios ou Apelidos: <select onchange="choose_name(this.value)" >;
+                text += <option value="0">Choose an option:</option>;
+                text += <option value=${JSON.stringify(obj[fn])}> Nomes Próprios </option>;
+                text += <option value=${JSON.stringify(obj[ln])}> Apelidos </option>;
+                text += </select> ;
                 document.getElementById("table2").innerHTML = text;
             }
 
@@ -306,7 +346,7 @@ if __name__ == "__main__":
                 text = "";
                 
                 for (let f in array){
-                    text += `${f} :: ${array[f]}, <br />`
+                    text += ${f} :: ${array[f]}, <br />
                 }
 
                 document.getElementById("table3").innerHTML = text;
@@ -319,3 +359,4 @@ if __name__ == "__main__":
 ])
     with open("index.html", "w") as outfile:
         outfile.write(html)
+Comenta todas as linhas a explicar o que cada uma faz
