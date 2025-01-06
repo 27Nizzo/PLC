8994268a7d31acab266d5c6c2dc0ec60f128d7fa
Afonso Martins <88838373+27Nizzo@users.noreply.github.com>
2025-01-06 23:45:42 +0000

    Update parser.py

--- PLC/TP's/TP2/parser.py
+++ PLC/TP's/TP2/parser.py
@@ -1,18 +1,6 @@
 import ply.yacc as yacc
 from analex import lexer,LexerPLC
 
-data = '''
-PLC teste3
-
-var
-    a = 10;
-
-begin
-    b = a + 1;
-    write (b);
-end
-'''
-
 tokens = LexerPLC.tokens
 
 def p_programa(p):
@@ -23,15 +11,16 @@ def p_programa(p):
 
 def p_plc(p):
     '''
-    plc : PLC ID VAR declaracoes BEGIN comandos END'''
+    plc : PLC ID declaracoes BEGIN comandos END'''
     p[0] = f"START\n{p[4]}\n{p[6]}\nSTOP"
-
 
 def p_declaracoes(p):
     '''
     declaracoes : declaracao 
                 | declaracoes declaracao'''
-    if len(p) == 2:
+    if len(p) == 1:
+        p[0] = ""
+    elif len(p) == 2:
         p[0] = p[1]
     else:
         p[0] = f"{p[1]}\n{p[2]}"
@@ -39,13 +28,24 @@ def p_declaracoes(p):
 def p_declaracao(p):
     '''
     declaracao : ID SEMICOLON
-               | ID EQ INTEGER SEMICOLON 
-               | ID EQ leitura
+               | ID ASSIGN NUMBER SEMICOLON
+               | ID ASSIGN leitura
+               | ASSIGN STRING SEMICOLON 
     '''
+    # o ASSIGN STRING SEMICOLON lida com situações quando definimos strings em vars.
+
 
     if len(p) == 3:
         parser.registos[p[1]] = 0 
         p[0] = "PUSHI 0"
+    elif len(p) == 5:
+        if isinstance(p[3], int):
+            parser.registos[p[1]] = p[3]
+            p[0] = f"PUSHI {p[3]}"
+        else:
+            string_val = p[3][1:-1]
+            parser.registos[p[1]] = string_val
+            p[0] = f"PUSHS {p[3]}"
     else:
         parser.registos[p[1]] = p[3]
         p[0] = f"PUSHI {p[3]}"
@@ -351,17 +351,33 @@ parser.assembly = ""
 parser.registos = {}
 parser.success = True
 
-def compile(data):
-    result = parser.parse(lexer=lexer)
-    print(parser.assembly)
-    if parser.success:
-        print("Ficheiro lido com sucesso")
-        with open(f'teste.vm', 'w+') as f_out:
-            f_out.write(parser.assembly)
-            f_out.close()
-        print("Código assembly gerado e guardado.")
-    else:
-        print("Erro ao gerar o código.")
-    return result
 
-compile(data)
+
+def compile(filename):
+    try:
+        with open(filename, 'r') as file:
+            input_text = file.read()
+
+        result = parser.parse(input=input_text, lexer=lexer)
+        print(parser.assembly)
+        
+        if parser.success:
+            print("Ficheiro lido com sucesso")
+            output_filename = filename.replace('.plc', '.vm')
+            with open(output_filename, 'w+') as f_out:
+                f_out.write(parser.assembly)
+                f_out.close()
+            print("Código assembly gerado e guardado.")
+        else:
+            print("Erro ao gerar o código.")
+            
+        return result
+        
+    except FileNotFoundError:
+        print(f"Erro: O ficheiro {filename} não foi encontrado.")
+    except Exception as e:
+        print(f"Erro ao processar o ficheiro: {str(e)}")
+
+if __name__ == "__main__":
+    teste = "teste.plc"
+    compile(teste)
